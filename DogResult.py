8fa8a65fe43ca0016cff00975e304d424555e54b
Troy Murphy <tamurphy@ualberta.ca>
2020-09-07 23:10:00 -0600

    working
    fully functioning setup

---
+++ DogResult.py
@@ -0,0 +1,5 @@
+class DogResult:
+	def __init__(self, name, link, image):
+		self.name = name
+		self.link = link
+		self.imageLink = image
---
+++ emailer.py
@@ -0,0 +1,48 @@
+import smtplib, ssl
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from decouple import config
+
+def send_mail_using_gmail(dogList):
+	port = 587
+	password = config('API_PASSWORD', cast=str)
+
+	if password is None:
+		raise Exception("API Password not set")
+
+	context = ssl.create_default_context()
+	sender_email = "troymurphydev@gmail.com"
+	receiver_email = "troymurphy4@gmail.com"
+
+	with smtplib.SMTP(host='smtp.gmail.com', port=587) as s:
+		s.starttls()
+		s.login("troymurphydev@gmail.com", password)
+		# TODO: Send email here
+		message = MIMEMultipart("alternative")
+		message["Subject"] = "New pets on AARCS"
+		message["From"] = sender_email
+		message["To"] = config('EMAILTO', cast=str)
+
+		dogElems = [
+			"""\
+			<a href="{dog.link}">
+				<h4>{dog.name}</h4>
+				<img src="{dog.imageLink}"/>
+			</a>
+			<br/>
+			""".format(dog=x) for x in dogList
+		]
+		html = """\
+		<html>
+			<body>
+			<h1>{0} New Dogs Have Been Posted</h2>
+				{1}
+			</body>
+		</html>
+		""".format(len(dogList), "".join(dogElems))
+
+		message.attach(MIMEText(html, "html"))
+		s.sendmail(
+			sender_email, receiver_email, message.as_string()
+		)
